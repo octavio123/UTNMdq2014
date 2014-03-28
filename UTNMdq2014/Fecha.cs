a68adba81c6c61b8a18398b51be83b7a2a66cc46
someoneigna <someoneigna@gmail.com>
2014-03-28 17:38:50 -0300

    Cambios en clase Fecha
    Operadores de suma y resta.
    Implementación de Equal.

--- UTNMdq2014/Fecha.cs
+++ UTNMdq2014/Fecha.cs
@@ -8,12 +8,12 @@ namespace UTNMdq2014
     /// <summary>
     /// Contiene dia, mes y año.
     /// </summary>
-    class Fecha
+    public class Fecha : IEquatable<Fecha>
     {
         
-        public int Dia { set; get; }
-        public int Mes { set; get; }
-        public int Año { set; get; }
+        public int Dia { protected set; get; }
+        public int Mes { protected set; get; }
+        public int Año { protected set; get; }
 
         public Fecha()
         {
@@ -39,5 +39,25 @@ namespace UTNMdq2014
             return Dia + "/" + Mes + "/" + Año;
         }
 
+        public override Fecha operator+ (Fecha a, Fecha b)
+        {
+            return new Fecha(a.Dia + b.Dia, a.Mes + b.Mes, a.Año + b.Año);
+        }
+
+        public override Fecha operator- (Fecha a, Fecha b)
+        {
+            return new Fecha(a.Dia - b.Dia, a.Mes - b.Mes, a.Año - b.Año);
+        }
+
+        /// <summary>
+        /// Compara el dia, mes y año entre dos Fechas.
+        /// </summary>
+        /// <returns>Verdadero o falso en caso de que sean o no iguales.</returns>
+        public bool Equal (Fecha otra)
+        {
+            return (Dia == otra.Dia &&
+                     Mes == otra.Mes &&
+                     Año == otra.Año);
+        }
     }
 }
