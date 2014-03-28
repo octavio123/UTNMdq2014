2299078365206a00df25fd70d74f12e1759e6cb9
someoneigna <someoneigna@gmail.com>
2014-03-28 19:44:53 -0300

    Cambios Clase Examen.

--- UTNMdq2014/Examen.cs
+++ UTNMdq2014/Examen.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UTNMdq2014
+{
+    class Examen
+    {
+        public int Nota { get; set; }
+    }
+}
