8f31e2bd47d64bb498faf7d3e414a403e97fe1f9
Ignacio Alvarez <someoneigna@gmail.com>
2014-03-30 03:35:37 -0300

    Cambios en Clase Examen

--- UTNMdq2014/Examen.cs
+++ UTNMdq2014/Examen.cs
@@ -5,10 +5,24 @@ using System.Text;
 
 namespace UTNMdq2014
 {
-    class Examen
+    public class Examen
     {
         public int Nota { get; protected set; }
 
         public bool Parcial { get; protected set; }
+
+        public Materia MateriaCorrespondiente { get; protected set; }
+
+        public Examen (Materia materia, int nota, bool parcial)
+        {
+            if (nota <= 0 || nota > 10)
+                throw new ArgumentOutOfRangeException("nota", "La nota debe estar entre 1 y 10.");
+            if (materia == null)
+                throw new ArgumentNullException("materia", "Un examen debe pertenecer a una materia.");
+
+            Nota = nota;
+            Parcial = parcial;
+            MateriaCorrespondiente = materia;
+        }
     }
 }
