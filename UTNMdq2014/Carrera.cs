ea309079ef80307f200ccfc3cb1f7c6ac9ebbf43
Ignacio Alvarez <someoneigna@gmail.com>
2014-03-30 03:35:53 -0300

    Cambios en Clase Carrera.

--- UTNMdq2014/Carrera.cs
+++ UTNMdq2014/Carrera.cs
@@ -7,5 +7,24 @@ namespace UTNMdq2014
 {
     public class Carrera
     {
+        PlanEstudio plan;
+        public PlanEstudio Plan
+        {
+            get;
+            protected set;
+        }
+
+        public Carrera(PlanEstudio plan)
+        {
+            if (plan == null) 
+                throw new ArgumentNullException("plan", "El plan no puede ser nulo.");
+
+            Plan = plan;
+        }
+
+        public void CambiarPlan(PlanEstudio nuevo)
+        {
+            Plan = nuevo;
+        }
     }
 }
