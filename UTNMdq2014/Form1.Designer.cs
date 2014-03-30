b55c9a5defb88d60b7db557d9bff8f7e1f1eb582
Ignacio Alvarez <someoneigna@gmail.com>
2014-03-30 03:37:09 -0300

    Cambios en Clase Form1
    Código de prueba de interacción Materia - Requisito.

--- UTNMdq2014/Form1.Designer.cs
+++ UTNMdq2014/Form1.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(13, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(147, 238);
+            this.listBox.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(179, 20);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(304, 230);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 262);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.RichTextBox textBox;
     }
 }
 
--- UTNMdq2014/Form1.cs
+++ UTNMdq2014/Form1.cs
@@ -14,6 +14,20 @@ namespace UTNMdq2014
         public Form1()
         {
             InitializeComponent();
+
+            Materia matematica, estadistica;
+            matematica = new Materia("Matematica", 1, 12);
+            estadistica = new Materia("Estadistica", 2, 24, new Requisito(matematica, true, false));
+            
+            Materia[] materias = new Materia[] { matematica, estadistica };
+            StringBuilder materiaInfo = new StringBuilder();
+
+            foreach (Materia mat in materias)
+            {
+                materiaInfo.Append(mat + "\n");
+            }
+
+            textBox.Text = materiaInfo.ToString();
         }
     }
 }
