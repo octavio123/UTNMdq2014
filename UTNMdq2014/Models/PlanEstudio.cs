6c2aaf210ddbdec3fc32c87217cf7419607818aa
someoneigna <someoneigna@gmail.com>
2014-03-31 16:41:36 -0300

    Cambios en Clase PlanEstudio.

--- UTNMdq2014/Models/PlanEstudio.cs
+++ UTNMdq2014/Models/PlanEstudio.cs
@@ -7,13 +7,28 @@ namespace UTNMdq2014.Models
 {
     public class PlanEstudio
     {
-        List<Materia> materias;
+        public List<Materia> Materias { get; set; }
 
         public PlanEstudio(List<Materia> listaMaterias)
         {
-            materias = listaMaterias;
+            Materias = listaMaterias;
         }
 
+        public PlanEstudio() : this(new List<Materia>())
+        {
+        }
 
+        public int PlanEstudioId { get; set; }
+
+        public void AgregarMateria(Materia materia)
+        {
+            if (materia == null)
+            {
+                string message = "La materia no puede ser nula.";
+                throw new ArgumentNullException("materia", message);
+            }
+            
+            Materias.Add(materia);
+        }
     }
 }
