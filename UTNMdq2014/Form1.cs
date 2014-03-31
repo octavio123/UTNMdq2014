09bef6f09b0d9a39697d0ffd263f006371eb7306
someoneigna <someoneigna@gmail.com>
2014-03-31 16:42:35 -0300

    Cambios en Form1.
    Prueba de los contenidos de la base de datos.

--- UTNMdq2014/Form1.cs
+++ UTNMdq2014/Form1.cs
@@ -16,26 +16,11 @@ namespace UTNMdq2014
         {
             InitializeComponent();
 
-            Materia matematica, estadistica;
-            matematica = new Materia("Matematica", 1, 12);
-            estadistica = new Materia("Estadistica", 2, 24, new Requisito(matematica, true, false));
-            
-            Materia[] materias = new Materia[] { matematica, estadistica };
-            StringBuilder materiaInfo = new StringBuilder();
-
-            foreach (Materia mat in materias)
-            {
-                materiaInfo.Append(mat + "\n");
-            }
-
-            textBox.Text = materiaInfo.ToString();
-
             FacultadDbContext dbContext = new FacultadDbContext();
-
-            foreach(var alumno in dbContext.Alumnos)
-            {
-                textBox.Text += alumno + "\n";
-            }
+            textBox.Text += dbContext.Alumnos.ToList()[0] + "\n" +
+                            dbContext.Materias.ToList()[0] + "\n" +
+                            dbContext.Profesores.ToList()[0] + "\n" +
+                            dbContext.Legajos.ToList()[0] + "\n";
 
             dbContext.Dispose();
         }
