8326e9ff4dd089a4e4aa16b1fd1bd98186f65f28
someoneigna <someoneigna@gmail.com>
2014-04-08 15:00:09 -0300

    Creada Clase Usuario.

---
+++ UTNMdq2014/Modelos/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UTNMdq2014.Modelos
+{
+
+
+    public enum UsuarioTipo // UserType :P
+    {
+        Profesor, Administrador, Mantenedor, Consultor
+    }
+
+    public class Usuario
+    {
+        public UsuarioTipo Tipo { get; set; }
+        public string Nombre { get; set; }
+        public string Contraseña {get; set; }
+        
+        private string salt;
+
+        public Usuario(string nombre, string contraseña)
+        {
+            Nombre = nombre;
+            Contraseña = contraseña; // PasswordHash h = PasswordHasher.Hash(contraseña); 
+                                     // Contraseña = h.Password;
+                                     // salt = h.Salt;
+        }
+    }
+}
