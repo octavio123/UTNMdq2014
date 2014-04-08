0ee2d2cfe1437e58d637f4bd7378a77fe404b30b
someoneigna <someoneigna@gmail.com>
2014-04-08 14:48:46 -0300

    Creadas Clase repositorio de Alumno, Materia y Profesor (Archivos XML)

---
+++ UTNMdq2014/Datos/MateriasRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UTNMdq2014.Modelos;
+
+namespace UTNMdq2014.Datos
+{
+    public class MateriasRepositorio : Repositorio<Materia>
+    {
+        public List<Materia> Materias { get; set; }
+        static string RepoFile = "materias.xml";
+        
+        public MateriasRepositorio() : base(RepoFile)
+        {
+            Materias = base.Datos;
+        }
+    }
+}
---
+++ UTNMdq2014/Datos/Repositorio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace UTNMdq2014.Datos
+{
+    public class Repositorio<T> : IDisposable
+    {
+        public List<T> Datos { get; set; }
+        string file;
+
+        protected Repositorio(string filename)
+        {
+            file = filename;
+            this.Datos = new List<T>();
+        }
+
+        protected void Save()
+        {
+            using (XmlWriter writer = XmlWriter.Create(file))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T[]));
+                serializer.Serialize(writer, Datos.ToArray());
+            }
+        }
+
+        protected void Load()
+        {
+            if (File.Exists(file))
+            {
+                using (XmlReader reader = XmlReader.Create(file))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(T[]));
+                    foreach (var elemento in serializer.Deserialize(reader) as T[])
+                    {
+                        Datos.Add(elemento);
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Save();
+        }
+    }
+}
