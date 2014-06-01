34b3346abd237b932df83023d683882953a86bfd
acrerega <adrian.rega@hotmail.com>
2014-06-01 19:57:05 -0300

    Update Aula.cs

--- UTNMdq2014/Modelos/Aula.cs
+++ UTNMdq2014/Modelos/Aula.cs
@@ -7,5 +7,28 @@ namespace UTNMdq2014.Modelos
 {
     public class Aula
     {
+        public string Nombre {set; get;}
+        public int Capacidad {set; get;}
+        public int Computadoras {set; get;}
+        public bool Proyector {set; get}
+        
+        int piso;    
+        string sector;
+        
+        public Aula()
+        {
+            Nombre =""";
+        }
+        
+        public Aula (string nombre, int cap, int comp, int piso, string sector, bool proyector)
+        {
+            Nombre = nombre;
+            Capacidad = cap;
+            Computadoras = comp;
+            this.piso = piso;
+            this.sector = sector;
+            Proyector = proyector;
+        }
+    
     }
 }
