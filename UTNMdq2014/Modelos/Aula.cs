33296305efb744db88123c36bee8be32f670e6ce
acrerega <adrian.rega@hotmail.com>
2014-06-01 21:56:19 -0300

    Clase Aula

--- UTNMdq2014/Modelos/Aula.cs
+++ UTNMdq2014/Modelos/Aula.cs
@@ -7,17 +7,36 @@ namespace UTNMdq2014.Modelos
 {
     public class Aula
     {
-        public string Nombre {set; get;}
-        public int Capacidad {set; get;}
-        public int Computadoras {set; get;}
-        public bool Proyector {set; get}
+        private string nombre, sector;
+        private int capacidad, cantidadComputadoras, piso;
+        private bool proyector;
+
+        #region Propiedades
+
+        public string Nombre {set {nombre = value;} get { return nombre;}}
         
-        int piso;    
-        string sector;
+        public int Capacidad {set {capacidad = value;} get {return capacidad;}}
         
+        public int Computadoras {set{cantidadComputadoras = value} get{return cantidadComputadoras;}}
+        
+        public bool Proyector { set { proyector = value; } get { return proyector; } }
+
+        public int Piso { set { piso = value; } get { return piso; } }
+
+        public string Sector { set { sector = value; } get { return sector; } }
+        
+        #endregion
+
+        #region Contructores
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
         public Aula()
         {
-            Nombre =""";
+            Nombre ="";
+            Proyector = false;
         }
         
         public Aula (string nombre, int cap, int comp, int piso, string sector, bool proyector)
@@ -25,10 +44,26 @@ namespace UTNMdq2014.Modelos
             Nombre = nombre;
             Capacidad = cap;
             Computadoras = comp;
-            this.piso = piso;
-            this.sector = sector;
+            Piso = piso;
+            Sector = sector;
             Proyector = proyector;
         }
-    
+
+        public Aula(Aula a)
+        {
+            Nombre = a.Nombre;
+            Capacidad = a.Capacidad;
+            Computadoras = a.Computadoras;
+            Piso = a.Piso;
+            Sector = a.Sector;
+            Proyector = a.Proyector;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        #endregion
+
     }
 }
--- UTNMdq2014/Modelos/Horario.cs
+++ UTNMdq2014/Modelos/Horario.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UTNMdq2014.Modelos
+{
+    public class Horario
+    {
+    }
+}
